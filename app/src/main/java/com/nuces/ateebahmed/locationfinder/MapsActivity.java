bd9f1daa103a16e0ce1b35f4545809ba7dab9158
ateebahmed <k132252@nu.edu.pk>
2016-11-10 21:05:10 +0500

    Fixed maps and location service, needs to check on actual device

--- app/src/main/java/com/nuces/ateebahmed/locationfinder/MapsActivity.java
+++ app/src/main/java/com/nuces/ateebahmed/locationfinder/MapsActivity.java
@@ -1,8 +1,19 @@
 package com.nuces.ateebahmed.locationfinder;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,20 +21,33 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient gClient;
+    private LocationRequest locReq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        gClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        gClient.connect();
+
+        locReq = new LocationRequest();
+        locReq.setInterval(1000);
+        locReq.setFastestInterval(1000);
+        locReq.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locReq.setSmallestDisplacement(0);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
-
 
     /**
      * Manipulates the map once available.
@@ -37,10 +61,35 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setIndoorEnabled(false);
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(0, 0);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        LocationServices.FusedLocationApi.requestLocationUpdates(gClient, locReq, this);
+        Log.i("MAPS", "connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        gClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("MAPS", connectionResult.getErrorCode() + "");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng pos = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(pos));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
+        Log.i("MAPS", "changed");
     }
 }
--- app/src/main/java/com/nuces/ateebahmed/locationfinder/Tracker.java
+++ app/src/main/java/com/nuces/ateebahmed/locationfinder/Tracker.java
@@ -1,26 +1,43 @@
 package com.nuces.ateebahmed.locationfinder;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+import android.content.IntentSender;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Build;
+import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
-import android.Manifest;
 import android.widget.Toast;
 
-public class Tracker extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+
+public class Tracker extends AppCompatActivity implements ConnectionCallbacks,
+        OnConnectionFailedListener, LocationListener, ResultCallback<LocationSettingsResult> {
 
     private TextView etLoc;
-    private LocationManager locManager;
-    private LocationListener locListener;
+    protected GoogleApiClient gClient;
+    protected Location loc;
+    protected LocationRequest locRequest;
+    protected boolean reqLocUpds;
+    protected static final int CHECK_SETTINGS = 0x0;
+    protected LocationSettingsRequest locSettingsReq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,81 +46,166 @@ public class Tracker extends AppCompatActivity {
 
         etLoc = (TextView) findViewById(R.id.etLoc);
 
-        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                etLoc.setText(location.getLatitude() + " : " + location.getLongitude());
-            }
+        reqLocUpds = false;
+        buildGClient();
+        createLocRequest();
+        buildLocSettingsReq();
+        checkLocSettings();
+    }
 
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-                Toast.makeText(Tracker.this, "Enable Location service", Toast.LENGTH_SHORT).show();
-                Intent access = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(access);
-            }
-        };
+    protected synchronized void buildGClient() {
+        gClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(LocationServices.API).build();
+        reqLocUpds = true;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        requestLocation();
+        gClient.connect();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (locManager != null) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) ;
-            locManager.removeUpdates(locListener);
-        }
+        if (gClient.isConnected())
+            gClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Toast.makeText(this, "GClient connected", Toast.LENGTH_SHORT).show();
+        /*if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+            return;
+        }*/
+        if (loc == null)
+            loc = LocationServices.FusedLocationApi.getLastLocation(gClient);
+        if (reqLocUpds)
+            startLocUpds();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, "GClient disconnected", Toast.LENGTH_SHORT).show();
+        gClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("TRACKER", connectionResult.getErrorCode() + "");
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case 1:
-                requestLocation();
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
         }
     }
 
-    private void requestLocation() {
-        if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER) &&
-                !locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            Toast.makeText(this, "Enable Location service", Toast.LENGTH_SHORT).show();
-            Intent access = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-            startActivity(access);        } else {
-            if (locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
-                viaProvider(LocationManager.NETWORK_PROVIDER);
-            else if (locManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
-                viaProvider(LocationManager.GPS_PROVIDER);
-        }
+    @Override
+    public void onLocationChanged(Location location) {
+        loc = location;
+        Log.e("TRACKER", loc.getLatitude() + ":" + loc.getLongitude());
+        etLoc.setText(loc.getLatitude() + ":" + loc.getLongitude());
+
     }
 
-    private void viaProvider(String provider) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[] {
-                        Manifest.permission.ACCESS_COARSE_LOCATION,
-                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET
-                }, 1);
+    protected void createLocRequest() {
+        locRequest = new LocationRequest();
+        locRequest.setInterval(1000);
+        locRequest.setFastestInterval(1000);
+        locRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locRequest.setSmallestDisplacement(0);
+    }
+
+    private void startLocUpds() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(gClient, locRequest, this)
+        .setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                reqLocUpds = true;
             }
-            return;
+        });
+    }
+
+    private void stopLocUpds() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(gClient, this)
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        reqLocUpds = false;
+                    }
+                });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (gClient.isConnected())
+            stopLocUpds();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (gClient.isConnected() && reqLocUpds)
+            startLocUpds();
+    }
+
+    @Override
+    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+        final Status status = locationSettingsResult.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                Log.i("Tracker", "Location enabled");
+                startLocUpds();
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                Log.e("Tracker", "Location disabled");
+                try{
+                    status.startResolutionForResult(this, CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e("Tracker", e.getMessage());
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                Log.e("Tracker", "Location disabled. No dialog shown");
+                break;
         }
-        locManager.requestLocationUpdates(provider, 0, 0, locListener);
+    }
+
+    protected void buildLocSettingsReq() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locRequest);
+        locSettingsReq = builder.build();
+    }
+
+    protected void checkLocSettings() {
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi
+                .checkLocationSettings(gClient, locSettingsReq);
+        result.setResultCallback(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CHECK_SETTINGS:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        Log.i("Tracker", "Settings changed");
+                        startLocUpds();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.i("Tracker", "Settings not changed");
+                        break;
+                }
+                break;
+        }
     }
 }
