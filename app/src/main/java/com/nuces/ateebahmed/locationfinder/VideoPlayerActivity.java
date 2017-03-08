dc9ceb1470bf4b2f1cdb78a3f57549798f0e8c3a
ateebahmed <k132252@nu.edu.pk>
2017-03-09 03:50:41 +0500

    Add ImagePreview before sending and VideoPlayer. MapsActivity checks if Location is enabled or disabled and ask to turn on location provider.

---
+++ app/src/main/java/com/nuces/ateebahmed/locationfinder/VideoPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.nuces.ateebahmed.locationfinder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+    }
+}
